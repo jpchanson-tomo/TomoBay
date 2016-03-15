56716c589aeec7480d56551e9105ffc856631491
jan hanson <jpchansondev@gmail.com>
2016-03-15 11:51:42 +0000

    ?!?!?!?!?!?!?!?

--- src/tomoBay/MAIN.java
+++ src/tomoBay/MAIN.java
@@ -1,7 +1,6 @@
 package tomoBay;
 import org.apache.log4j.Logger;
 
-import tomoBay.model.dataTypes.PartList;
 import tomoBay.model.dataTypes.ServerStatus;
 import tomoBay.model.services.ServiceFactory;
 import tomoBay.model.services.ServiceFactory.ConfiguredServiceType;
@@ -28,22 +27,22 @@ public final class MAIN
 		server.start(1337);
 		ServerStatus.instance().setStatus(ServerStatus.RunLevel.RUNNING);
 //////		
-//		final ServiceScheduler services = new ServiceScheduler(5);
-//		services.add(ServiceFactory.make(ServiceFactory.ServiceType.EBAY_SERVICE));
-//		services.add(ServiceFactory.make(ServiceFactory.ServiceType.OUT_OF_HOURS_SERVICE));
-//		services.add(ServiceFactory.make(ServiceFactory.ServiceType.RESCAN_ERRORS_SERVICE));
-//		services.add(ServiceFactory.make(ServiceFactory.ServiceType.CHECK_ERRORS));
-//		final String data = "<EMAIL>"
-//				+ "<TO>tomomotorbay@gmail.com</TO>"
-//				+ "<TO>paul@tomoparts.co.uk</TO>"
-//				+ "<TO>steve@tomoparts.co.uk</TO>"
-//				+ "<SUBJECT>ERRORS TO FIX!!!!!</SUBJECT>"
-//				+ "</EMAIL>";
-//		services.add(ServiceFactory.make(
-//										ConfiguredServiceType.EMAIL_ERRORS_SERVICE,
-//										new EmailErrorsConfig().configure(data)
-//										));
-//		services.start(20);
+		final ServiceScheduler services = new ServiceScheduler(5);
+		services.add(ServiceFactory.make(ServiceFactory.ServiceType.EBAY_SERVICE));
+		services.add(ServiceFactory.make(ServiceFactory.ServiceType.OUT_OF_HOURS_SERVICE));
+		services.add(ServiceFactory.make(ServiceFactory.ServiceType.RESCAN_ERRORS_SERVICE));
+		services.add(ServiceFactory.make(ServiceFactory.ServiceType.CHECK_ERRORS));
+		final String data = "<EMAIL>"
+				+ "<TO>tomomotorbay@gmail.com</TO>"
+				+ "<TO>paul@tomoparts.co.uk</TO>"
+				+ "<TO>steve@tomoparts.co.uk</TO>"
+				+ "<SUBJECT>ERRORS TO FIX!!!!!</SUBJECT>"
+				+ "</EMAIL>";
+		services.add(ServiceFactory.make(
+										ConfiguredServiceType.EMAIL_ERRORS_SERVICE,
+										new EmailErrorsConfig().configure(data)
+										));
+		services.start(20);
 		
 		
 	}
