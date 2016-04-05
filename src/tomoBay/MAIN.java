f2cdc69dc5cebe3fe6489d94eed62920a5563537
jan hanson <jpchansondev@gmail.com>
2016-04-05 08:24:40 +0100

    and forgot to uncomment the services

--- src/tomoBay/MAIN.java
+++ src/tomoBay/MAIN.java
@@ -2,11 +2,10 @@ package tomoBay;
 import org.apache.log4j.Logger;
 
 import tomoBay.model.dataTypes.ServerStatus;
-import tomoBay.model.dataTypes.conditionalStatement.Condition;
-import tomoBay.model.dataTypes.conditionalStatement.False;
-import tomoBay.model.dataTypes.conditionalStatement.Conditional;
-import tomoBay.model.dataTypes.conditionalStatement.Result;
-import tomoBay.model.dataTypes.conditionalStatement.True;
+import tomoBay.model.services.ServiceFactory;
+import tomoBay.model.services.ServiceFactory.ConfiguredServiceType;
+import tomoBay.model.services.ServiceScheduler;
+import tomoBay.model.services.emailErrorsService.EmailErrorsConfig;
 import tomoBay.view.HttpServer;
 /**
  * The entry point into the program, this is a stopgap solution to get invoices ,of orders that
@@ -27,23 +26,23 @@ public final class MAIN
 		final HttpServer server = new HttpServer();
 		server.start(1337);
 		ServerStatus.instance().setStatus(ServerStatus.RunLevel.RUNNING);
-//		
-//		final ServiceScheduler services = new ServiceScheduler(5);
-//		services.add(ServiceFactory.make(ServiceFactory.ServiceType.EBAY_SERVICE));
-//		services.add(ServiceFactory.make(ServiceFactory.ServiceType.OUT_OF_HOURS_SERVICE));
-//		services.add(ServiceFactory.make(ServiceFactory.ServiceType.RESCAN_ERRORS_SERVICE));
-//		services.add(ServiceFactory.make(ServiceFactory.ServiceType.CHECK_ERRORS));
-//		final String data = "<EMAIL>"
-//				+ "<TO>tomomotorbay@gmail.com</TO>"
+		
+		final ServiceScheduler services = new ServiceScheduler(5);
+		services.add(ServiceFactory.make(ServiceFactory.ServiceType.EBAY_SERVICE));
+		services.add(ServiceFactory.make(ServiceFactory.ServiceType.OUT_OF_HOURS_SERVICE));
+		services.add(ServiceFactory.make(ServiceFactory.ServiceType.RESCAN_ERRORS_SERVICE));
+		services.add(ServiceFactory.make(ServiceFactory.ServiceType.CHECK_ERRORS));
+		final String data = "<EMAIL>"
+				+ "<TO>tomomotorbay@gmail.com</TO>"
 //				+ "<TO>paul@tomoparts.co.uk</TO>"
-//				+ "<TO>steve@tomoparts.co.uk</TO>"
-//				+ "<SUBJECT>ERRORS TO FIX!!!!!</SUBJECT>"
-//				+ "</EMAIL>";
-//		services.add(ServiceFactory.make(
-//										ConfiguredServiceType.EMAIL_ERRORS_SERVICE,
-//										new EmailErrorsConfig().configure(data)
-//										));
-//		services.start(20);
+				+ "<TO>steve@tomoparts.co.uk</TO>"
+				+ "<SUBJECT>ERRORS TO FIX!!!!!</SUBJECT>"
+				+ "</EMAIL>";
+		services.add(ServiceFactory.make(
+										ConfiguredServiceType.EMAIL_ERRORS_SERVICE,
+										new EmailErrorsConfig().configure(data)
+										));
+		services.start(20);
 		
 	}
 }
